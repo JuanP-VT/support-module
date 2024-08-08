f9bed8f5abb9478a45ffd75153f4e07496c00ffc
Axel Osbaldo Sánchez López <asanchez@pktuno.com>
2024-08-08 12:17:37 -0700

    ADD module constants

---
+++ constants/ModuloSoporte.js
@@ -0,0 +1,12 @@
+export const statusList = {
+  COMPLETADO: "Completado",
+  ENPROGRESO: "En progreso",
+  PENDIENTE: "Pendiente",
+  CANCELADO: "Cancelado",
+};
+
+export const typeList = {
+  CANCELACIÓN: "Cancelación",
+  REGISTRO: "Registro",
+  SOPORTE: "Soporte",
+};
