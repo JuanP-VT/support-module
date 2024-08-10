9a551b87f14fe844ee0664f6eb8158277275d711
Pablo <cv.juanp@gmail.com>
2024-08-09 17:50:47 -0700

    Add mapper example

--- mapper/apiMock.js
+++ mapper/apiMock.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-unused-vars */
+/**
+ * Las respuestas del backend suelen contener propiedades mal nombradas como "ciudadO, tokenXUsuario etc"
+ * En su momento larry implementó estas clases "mappers para renombrar las respuestas del backend
+ * También puede implementar su propio metodos como getFullName etc
+ */
 export const dataMock = [
   {
     ID: 1,
@@ -14,3 +20,54 @@ export const dataMock = [
     ciudadO: "ciudad 1",
   },
 ];
+
+class Address {
+  constructor(street, avenue, city) {
+    this.street = street;
+    this.avenue = avenue;
+    this.city = city;
+  }
+  static fromJson(obj) {
+    return new Address(obj["calle"], obj["avenida"], obj["ciudad"]);
+  }
+}
+
+export class User {
+  constructor(
+    id,
+    name,
+    lastName,
+    email,
+    address,
+    phone,
+    dateOfBirth,
+    state,
+    originCity
+  ) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.address = address;
+    this.phone = phone;
+    this.dateOfBirth = dateOfBirth;
+    this.state = state;
+    this.originCity = originCity;
+  }
+  static fromJson(obj) {
+    return new User(
+      obj["ID"],
+      obj["nombr"],
+      obj["apellidO"],
+      obj["correO"],
+      obj["direcciones"].map((item) => Address.fromJson(item)),
+      obj["telefonos"],
+      obj["fechaN"],
+      obj["estado"],
+      obj["ciudadO"]
+    );
+  }
+  get fullName() {
+    return `${this.name} ${this.lastName}`;
+  }
+}
