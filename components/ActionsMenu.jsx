b31066883720d902798b8d158f97cb117d9f8fe1
Pablo <cv.juanp@gmail.com>
2024-08-14 15:15:06 -0700

    UPD actionmenu props

--- components/ActionsMenu.jsx
+++ components/ActionsMenu.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import * as React from "react";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
@@ -6,15 +5,17 @@ import MenuItem from "@mui/material/MenuItem";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Edit, Delete } from "@mui/icons-material";
 import PropTypes from "prop-types";
-import { useDispatch,  } from "react-redux";
-import {openDeleteDialog, openEditDialog, setSelectedItem, } from "../redux/features/dataSlice"
-
-const ITEM_HEIGHT = 48;
+import { useDispatch } from "react-redux";
+import {
+  openDeleteDialog,
+  openEditDialog,
+  setSelectedItem,
+} from "../redux/features/dataSlice";
 
 export default function ActionsMenu({ supportItem }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
-  
+
   const dispatch = useDispatch();
 
   const handleClick = (event) => {
@@ -36,34 +37,24 @@ export default function ActionsMenu({ supportItem }) {
       >
         <MoreVertIcon />
       </IconButton>
-      <Menu
-        id="long-menu"
-        MenuListProps={{
-          "aria-labelledby": "long-button",
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5,
-            width: "20ch",
-          },
-        }}
-      >
-        <MenuItem onClick={()=>{
-          dispatch(setSelectedItem(supportItem))
-          dispatch(openEditDialog())
-          handleClose()
-        } }>
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <MenuItem
+          onClick={() => {
+            dispatch(setSelectedItem(supportItem));
+            dispatch(openEditDialog());
+            handleClose();
+          }}
+        >
           <Edit />
-          Editar          
+          Editar
         </MenuItem>
-        <MenuItem onClick={()=>{
-          dispatch(setSelectedItem(supportItem))
-          dispatch(openDeleteDialog())
-          handleClose()
-        }}>
+        <MenuItem
+          onClick={() => {
+            dispatch(setSelectedItem(supportItem));
+            dispatch(openDeleteDialog());
+            handleClose();
+          }}
+        >
           <Delete />
           Cancelar
         </MenuItem>
@@ -73,6 +64,7 @@ export default function ActionsMenu({ supportItem }) {
 }
 
 ActionsMenu.propTypes = {
+  supportItem: PropTypes.object.isRequired,
   tipo: PropTypes.string,
   fecha: PropTypes.string,
   usuario: PropTypes.string,
